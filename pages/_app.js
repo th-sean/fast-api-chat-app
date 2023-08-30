eae8a67a8191cfc0d0dbef562aff2dff91533af5
sean3687 <129122794+th-sean@users.noreply.github.com>
2023-08-30 14:28:04 -0700

    login page

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,23 @@
 import "../styles/globals.css";
 import HamburgerNavbar from "../components/navbar";
 import DesktopDrawer from "../components/desktopDrawer";
+import { useRouter } from 'next/router';
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+  
+  const showNavigation = router.pathname !== '/login';
+
   return (
     <div className="flex flex-col h-screen lg:flex-row">
-      <div className="hidden lg:block">
-        <DesktopDrawer />
-      </div>
-      <HamburgerNavbar className="lg:hidden fixed top-0" />
+      {showNavigation && (
+        <div className="hidden lg:block">
+          <DesktopDrawer />
+        </div>
+      )}
+      {showNavigation && (
+        <HamburgerNavbar className="lg:hidden fixed top-0" />
+      )}
       <div className="w-full overflow-y-auto bg-slate-100">
         <div className="flex-grow overflow-y-auto ">
           <Component {...pageProps} />
