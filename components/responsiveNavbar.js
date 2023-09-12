984ded75c70e796c8f553da3d4297fae8f46f4b4
th-sean <129122794+th-sean@users.noreply.github.com>
2023-09-12 15:48:41 -0700

    Update responsiveNavbar.js

--- components/responsiveNavbar.js
+++ components/responsiveNavbar.js
@@ -113,7 +113,7 @@ const Navbar = () => {
       <div className="hidden lg:block relative h-screen overflow-y-hidden w-64 bg-white shadow-md z-10 transition-transform transform translate-x-0 transition duration-300 flex flex-col">
         <div className="w-full h-40">
           <Image
-            src="/Assets/logo.png"
+            src="/assets/logo.png"
             alt="My Image"
             width={300}
             height={200}
