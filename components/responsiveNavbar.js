84094c80a38b150297065c55c0e9559d61da96ac
sean3687 <129122794+th-sean@users.noreply.github.com>
2023-09-13 16:16:39 -0700

    navbar update

--- components/responsiveNavbar.js
+++ components/responsiveNavbar.js
@@ -7,11 +7,18 @@ import {
   AiOutlineCloudUpload,
   AiOutlineLogin,
 } from "react-icons/ai";
+
+import {
+  PiUploadDuotone,
+  PiChatTeardropTextDuotone,
+  PiUserCircleDuotone,
+} from "react-icons/pi";
 import { BsChat } from "react-icons/bs";
 import Image from "next/image";
 import useAccountInfoStore from "../stores/store.js";
 import axios from "axios";
 import { useRouter } from "next/router";
+import firstLetterCapitalized from "../utils/stringManimupaltion.js";
 
 async function getProfile(setLabelArray, setUsername) {
   const response = await axios.get("/api/getProfile", {
@@ -32,37 +39,48 @@ async function getProfile(setLabelArray, setUsername) {
 
 const tabs = [
   {
-    icon: <AiOutlineCloudUpload className="text-xl" />,
-    text: "Upload Documents",
+    icon: <PiUploadDuotone className="w-4 h-4" />,
+    text: "Knowledge base",
     link: "/upload",
   },
   {
-    icon: <BsChat className="text-xl" />,
+    icon: <PiChatTeardropTextDuotone className="w-4 h-4" />,
     text: "Chatbot",
     link: "/chatbot",
   },
   {
-    icon: <AiOutlineUser className="text-xl" />,
+    icon: <PiUserCircleDuotone className="w-4 h-4" />,
     text: "Account",
     link: "/profile",
   },
 ];
 
 const TabItems = ({ setSelectedTabIndex, selectedTabIndex }) => {
-  return tabs.map((tab, index) => (
-    <div
-      key={index}
-      className={`${
-        selectedTabIndex === index ? "bg-blue-600 text-white" : ""
-      } flex items-center justify-center align-center font-medium p-3 rounded-lg hover:bg-gray-200  transition duration-300 m-2`}
-      onClick={() => setSelectedTabIndex(index)}
-    >
-      <div className="ml-3 text-sm mr-3">{tab.icon}</div>
-      <Link href={tab.link} className="w-full text-sm">
-        {tab.text}
-      </Link>
+  return (
+    <div>
+      <div className="flex justify-center align-middle px-5 pt-4 pb-3">
+        <button className="transition-all duration-200 relative font-semibold shadow-sm outline-none hover:outline-none focus:outline-none rounded-md px-3 py-1.5 text-sm bg-blue-600 text-white ring-0 ring-blue-600 hover:ring-2 active:ring-0 w-full">
+          + Create content
+        </button>
+      </div>
+      {tabs.map((tab, index) => (
+        <div
+          key={index}
+          className={`${
+            selectedTabIndex === index
+              ? "text-blue-800 text-sm font-medium"
+              : ""
+          } mx-4 flex py-2 items-center justify-center align-center rounded hover:bg-gray-100 font-medium transition duration-300`}
+          onClick={() => setSelectedTabIndex(index)}
+        >
+          <div className="ml-3 text-sm mr-3">{tab.icon}</div>
+          <Link href={tab.link} className="w-full text-sm">
+            {tab.text}
+          </Link>
+        </div>
+      ))}
     </div>
-  ));
+  );
 };
 
 function InterestItems() {
@@ -76,14 +94,14 @@ function InterestItems() {
 
   return (
     <div className="p-2 flex flex-wrap">
-      {labelArray.map((label, index) => (
+      {/* {labelArray.map((label, index) => (
         <div
           className="bg-blue-500 rounded text-white text-xs p-1 m-1"
           key={index}
         >
           # {label}
         </div>
-      ))}
+      ))} */}
     </div>
   );
 }
@@ -109,35 +127,29 @@ const Navbar = () => {
   return (
     <div>
       {/* Desktop Navigation */}
-      <div className="hidden lg:block relative h-screen overflow-y-hidden w-64 bg-white shadow-md z-10 transition-transform transform translate-x-0 transition duration-300 flex flex-col">
-        <div className="w-full h-40">
-          <img
-            src="/logo.png"
-            alt="My Image"
-            width={300}
-            height={200}
+      <div className="">
+        <div className="flex items-center justify-between h-12 px-4 border-gray-200 hover:bg-gray-100 border-b">
+          <div className="flex items-center ">
+            <div className="bg-green-800 text-xs w-6 h-6 aspect-1 rounded-full font-bold text-white flex items-center justify-center">
+              {firstLetterCapitalized(username)}
+            </div>
+            <div className="text-black-800 truncate ml-2 font-medium">
+              {username}
+            </div>
+          </div>
+          <Link href={"/login"} className="text-sm">
+            <AiOutlineLogin className="text-xl" />
+          </Link>
+        </div>
+      </div>
+      <div className="hidden lg:block relative h-screen overflow-y-hidden w-64 bg-white z-10 transition-transform transform translate-x-0 transition duration-300 flex flex-col">
+        <div className="pb-2 flex-0 border-b border-gray-200 mt-2 ">
+          <TabItems
+            setSelectedTabIndex={setSelectedTabIndex}
+            selectedTabIndex={selectedTabIndex}
           />
         </div>
-        <TabItems
-          setSelectedTabIndex={setSelectedTabIndex}
-          selectedTabIndex={selectedTabIndex}
-        />
-
-        <div className="absolute mb-4 bottom-0">
-          <InterestItems />
-
-          <div className="flex justify-between bg-gray-100 p-3 m-3 rounded items-center shadow-lg">
-            <div className="flex items-center">
-              <div className="w-8 h-8 bg-black mr-4 items-center rounded">
-                <AiOutlineUser className="text-4xl fill-current bg-blue-400 rounded p-1 fill-white" />
-              </div>
-              <div className="text-sm">{username}</div>
-            </div>
-            <Link href={"/login"} className="text-sm">
-              <AiOutlineLogin className="text-xl" />
-            </Link>
-          </div>
-        </div>
+        <InterestItems />
       </div>
 
       {/* Mobile Navigation */}
--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,19 @@
 import "../styles/globals.css";
 import Navbar from "../components/responsiveNavbar"; // Import the merged Navbar
 import { useRouter } from 'next/router';
+import { Inter } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
 
 function MyApp({ Component, pageProps }) {
+    
     const router = useRouter();
 
     // Determine if the navigation should be shown based on the current route
     const showNavigation = router.pathname !== '/login' && router.pathname !== '/register';
 
     return (
-        <div className="flex flex-col h-screen lg:flex-row">
+        <div className={"flex flex-col h-screen lg:flex-row " +inter.className}>
             {showNavigation && <Navbar />}
             <div className="w-full overflow-y-auto bg-slate-100">
                 <div className="flex-grow overflow-y-auto">
