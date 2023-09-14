336b5affeb742926f4b1cf7811e7b9ea49bdf55a
unknown <sean.jin@teehub1.com>
2023-09-14 16:22:30 -0700

    navbar update

--- components/responsiveNavbar.js
+++ components/responsiveNavbar.js
@@ -13,6 +13,8 @@ import {
   PiChatTeardropTextDuotone,
   PiUserCircleDuotone,
 } from "react-icons/pi";
+
+import { FaFileLines, FaGoogleDrive } from "react-icons/fa6";
 import { BsChat } from "react-icons/bs";
 import Image from "next/image";
 import useAccountInfoStore from "../stores/store.js";
@@ -55,12 +57,18 @@ const tabs = [
   },
 ];
 
-const TabItems = ({ setSelectedTabIndex, selectedTabIndex, setShowCreateModal }) => {
-  
+const TabItems = ({
+  setSelectedTabIndex,
+  selectedTabIndex,
+  setShowCreateModal,
+}) => {
   return (
     <div>
       <div className="flex justify-center align-middle px-5 pt-4 pb-3">
-        <button className="transition-all duration-200 relative font-semibold shadow-sm outline-none hover:outline-none focus:outline-none rounded-md px-3 py-1.5 text-sm bg-blue-600 text-white ring-0 ring-blue-600 hover:ring-2 active:ring-0 w-full" onClick={setShowCreateModal(true)}>
+        <button
+          className="transition-all duration-200 relative font-semibold shadow-sm outline-none hover:outline-none focus:outline-none rounded-md px-3 py-1.5 text-sm bg-blue-600 text-white ring-0 ring-blue-600 hover:ring-2 active:ring-0 w-full"
+          onClick={() => setShowCreateModal(true)}
+        >
           + Create content
         </button>
       </div>
@@ -84,21 +92,49 @@ const TabItems = ({ setSelectedTabIndex, selectedTabIndex, setShowCreateModal })
   );
 };
 
-const CreateContentModal = ({ showModal, setShowModal }) => {
+const CreateContentModal = ({ showModal, setShowCreateModal }) => {
   return (
     showModal && (
       <div className="fixed top-0 left-0 w-full h-full flex justify-center items-center bg-gray-500 bg-opacity-50 z-50">
-        <div className="bg-white rounded p-8">
-          <h1 className="mb-2 text-lg font-semibold text-gray-900">My Modal</h1>
-          <p className="text-sm font-normal text-gray-600">This is the content of the modal.</p>
-          <div className="flex justify-end p-6"></div>
-          <button
-            className="transition-all duration-200 relative font-medium shadow-sm outline-none hover:outline-none focus:outline-none rounded-lg px-4 py-2 text-base bg-white text-gray-600 ring-1 ring-gray-200 hover:ring-2 active:ring-1"
-            onClick={() => setShowModal(false)}
-          >
-            <span className="flex items-center justify-center mx-auto space-x-2 select-none">Close Modal</span>
-            
-          </button>
+        <div className="bg-white rounded">
+          <div className="p-6 space-y-4 border-b">
+            <h1 className="mb-2 text-lg font-semibold text-gray-900">
+              Upload new content...
+            </h1>
+            <p className="text-sm font-normal text-gray-600">
+              You can upload document from local device or Google drive
+            </p>
+          </div>
+          <div className="grid grid-cols-2 gap-6 p-6 border-b">
+            <button className="p-4 ring-1 ring-gray-200 rounded-2xl text-left space-y-3 hover:ring-gray-300 active:ring-gray-400">
+              <div className="flex items-center space-x-3">
+                <FaFileLines className="w-4 h-4 text-blue-800" />
+                <div className="text-indigo-700 text-base font-semibold">
+                  + Upload document
+                </div>
+              </div>
+              <div>Upload your files from local device (.pdf, .png, .html)</div>
+            </button>
+            <button className="p-4 ring-1 ring-gray-200 rounded-2xl text-left space-y-3 hover:ring-gray-300 active:ring-gray-400">
+              <div className="flex items-center space-x-3">
+                <FaGoogleDrive className="w-4 h-4 text-red-800" />
+                <div className="text-red-700 text-base font-semibold">
+                  + Google drive
+                </div>
+              </div>
+              <div>Get your files securely from Google drive</div>
+            </button>
+          </div>
+          <div className="flex justify-end p-6">
+            <button
+              className="transition-all duration-200 relative font-medium shadow-sm outline-none hover:outline-none focus:outline-none rounded-lg px-4 py-2 text-base bg-white text-gray-600 ring-1 ring-gray-200 hover:ring-2 active:ring-1"
+              onClick={() => setShowCreateModal(false)}
+            >
+              <span className="flex items-center justify-center mx-auto space-x-2 select-none font-semibold">
+                Cancel
+              </span>
+            </button>
+          </div>
         </div>
       </div>
     )
@@ -147,24 +183,25 @@ const Navbar = () => {
   }, [token, router.pathname]);
 
   return (
-    <div>
+    <div className="">
       {/* Desktop Navigation */}
-      <div className="">
-        <div className="flex items-center justify-between h-12 px-4 border-gray-200 hover:bg-gray-100 border-b">
-          <div className="flex items-center ">
-            <div className="bg-green-800 text-xs w-6 h-6 aspect-1 rounded-full font-bold text-white flex items-center justify-center">
-              {firstLetterCapitalized(username)}
+
+      <div className="hidden lg:block relative h-screen overflow-y-hidden w-64 bg-gray-50 transition-transform transform translate-x-0 transition duration-300 flex flex-col">
+        <div className="">
+          <div className="flex items-center justify-between h-12 px-4 border-gray-200 hover:bg-gray-100 border-b">
+            <div className="flex items-center ">
+              <div className="bg-green-800 text-xs w-6 h-6 aspect-1 rounded-full font-bold text-white flex items-center justify-center">
+                {firstLetterCapitalized(username)}
+              </div>
+              <div className="text-black-800 truncate ml-2 font-medium">
+                {username}
+              </div>
             </div>
-            <div className="text-black-800 truncate ml-2 font-medium">
-              {username}
-            </div>
+            <Link href={"/login"} className="text-sm">
+              <AiOutlineLogin className="text-xl" />
+            </Link>
           </div>
-          <Link href={"/login"} className="text-sm">
-            <AiOutlineLogin className="text-xl" />
-          </Link>
         </div>
-      </div>
-      <div className="hidden lg:block relative h-screen overflow-y-hidden w-64 bg-white transition-transform transform translate-x-0 transition duration-300 flex flex-col">
         <div className="pb-2 flex-0 border-b border-gray-200 mt-2 ">
           <TabItems
             setSelectedTabIndex={setSelectedTabIndex}
@@ -173,6 +210,18 @@ const Navbar = () => {
           />
         </div>
         <InterestItems />
+
+        <div className="flex-grow overflow-y-auto">
+          <div className="text-sm text-gray-600 pl-5 pt-2">Chat</div>
+          <div className="flex justify-center align-middle px-5 pt-4 pb-3">
+            <button
+              className="transition-all duration-200 relative font-semibold  outline-none hover:outline-none focus:outline-none rounded-md px-3 py-1.5 text-sm border-gray-600 text-gray-500 ring-0 ring-gray-600 hover:ring-2 active:ring-0 w-full"
+              onClick={() => {}}
+            >
+              New conversation
+            </button>
+          </div>
+        </div>
       </div>
 
       {/* Mobile Navigation */}
@@ -183,17 +232,11 @@ const Navbar = () => {
             onClick={() => setDrawerOpen(false)}
           >
             <div className="absolute top-0 left-0 h-full w-64 bg-white shadow-md z-10 transition-transform transform translate-x-0 transition duration-300 custom:hidden">
-              <div className="w-full  h-40">
-                <Image
-                  src="/Assets/logo.png"
-                  alt="My Image"
-                  width={300}
-                  height={200}
-                />
-              </div>
+              
               <TabItems
                 setSelectedTabIndex={setSelectedTabIndex}
                 selectedTabIndex={selectedTabIndex}
+                setShowCreateModal={setShowCreateModal}
               />
             </div>
           </div>
@@ -214,10 +257,10 @@ const Navbar = () => {
 
         <div className="clear-both"></div>
       </div>
+
       <CreateContentModal
         showModal={showCreateModal}
         setShowCreateModal={setShowCreateModal}
-        
       />
     </div>
   );
