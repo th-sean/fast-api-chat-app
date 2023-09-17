78fadc5e3d4e103238efa7dbae5d04f1c646a192
sean3687 <129122794+th-sean@users.noreply.github.com>
2023-09-17 14:22:34 -0700

    test

--- components/layouts/singlePageLayout.js
+++ components/layouts/singlePageLayout.js
@@ -1,13 +1,19 @@
-
-
-function SinglePageLayout(children) {
-    return function (props) {
+function SinglePageLayout(WrappedComponent) {
+    function LayoutComponent(props) {
         return (
             <div>
-                {children}
+                <WrappedComponent {...props} />
             </div>
         );
-    };
+    }
+
+    LayoutComponent.displayName = `SinglePageLayout(${getDisplayName(WrappedComponent)})`;
+
+    return LayoutComponent;
+}
+
+function getDisplayName(Component) {
+    return Component.displayName || Component.name || 'Component';
 }
 
 export default SinglePageLayout;
